34ec7a6f3e17ac95acfca5ad43a37bd8f014bc60
koushikpuppala <koushikpuppala203@gmail.com>
2021-08-25 00:24:54 +0530

    Koushikpuppala

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -51,7 +51,11 @@ class Portfolio extends React.Component {
 												<p className='portfolio-item-description'>
 													{e.Description}
 												</p>
-												<a className='portfolio-item-link' href={e.Link}>
+												<a
+													className='portfolio-item-link'
+													target='_blank'
+													rel='noopener noreferrer'
+													href={e.Link}>
 													{e.LinkName}
 												</a>
 											</div>
