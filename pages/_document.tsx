6c79262c27bc7365c0b58d5a5c1181de5d542ef9
koushikpuppala <koushikpuppala203@gmail.com>
2022-06-09 17:23:09 +0530

    Koushikpuppala

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,8 +33,24 @@ const Document = () => {
 					crossOrigin='anonymous'
 					strategy='afterInteractive'
 				/>
+				<Script id='google-tag-manager' strategy='afterInteractive'>
+					{`
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-KM9WPPS');
+					`}
+				</Script>
 			</Head>
 			<body>
+				<noscript>
+					<iframe
+						src='https://www.googletagmanager.com/ns.html?id=GTM-KM9WPPS'
+						height='0'
+						width='0'
+						style={{ display: 'none', visibility: 'hidden' }}></iframe>
+				</noscript>
 				<Main />
 
 				<script
