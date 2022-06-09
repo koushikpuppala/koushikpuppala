8231442cacba0a917961f9eee6a4f8cfb4914edb
koushikpuppala <koushikpuppala203@gmail.com>
2022-06-09 18:44:13 +0530

    Koushikpuppala

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,37 +10,41 @@ const Document = () => {
 					rel='stylesheet'
 				/>
 
-				<Script
-					async
-					id='google-analytics-G-VFZBVWQG74'
-					src='https://www.googletagmanager.com/gtag/js?id=G-VFZBVWQG74'
-					crossOrigin='anonymous'
-					strategy='afterInteractive'
-				/>
-				<Script id='google-analytics' strategy='afterInteractive'>
-					{`
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', 'G-VFZBVWQG74');
-					`}
-				</Script>
-				<Script
+				<script
 					async
 					id='google-ca-pub-6510875727156988'
 					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6510875727156988'
 					crossOrigin='anonymous'
-					strategy='afterInteractive'
 				/>
-				<Script id='google-tag-manager' strategy='afterInteractive'>
+				<script
+					async
+					src='https://www.googletagmanager.com/gtag/js?id=G-VFZBVWQG74'
+					crossOrigin='anonymous'
+				/>
+				<script id='google-analytics'>
 					{`
-						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-						})(window,document,'script','dataLayer','GTM-KM9WPPS');
+						window.dataLayer = window.dataLayer || []
+						function gtag() {
+							dataLayer.push(arguments)
+						}
+						gtag('js', new Date())
+						gtag('config', 'G-VFZBVWQG74')
 					`}
-				</Script>
+				</script>
+				<script id='google-tag-manager'>
+					{`
+						;(function (w, d, s, l, i) {
+							w[l] = w[l] || []
+							w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+							var f = d.getElementsByTagName(s)[0],
+								j = d.createElement(s),
+								dl = l != 'dataLayer' ? '&l=' + l : ''
+							j.async = true
+							j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+							f.parentNode.insertBefore(j, f)
+						})(window, document, 'script', 'dataLayer', 'GTM-KM9WPPS')
+					`}
+				</script>
 			</Head>
 			<body>
 				<noscript>
