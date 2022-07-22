41620f10343a09fba8f9be8d1e68d2b6e6e90a4a
koushikpuppala <koushikpuppala203@gmail.com>
2022-07-22 10:25:50 +0530

    Koushikpuppala

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -16,8 +16,8 @@ const Contact = async (req: NextApiRequest, res: NextApiResponse) => {
 	const mailer = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-			user: `${process.env.EMAIL_USER}`,
-			pass: `${process.env.EMAIL_PASS}`,
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS,
 		},
 	})
 
