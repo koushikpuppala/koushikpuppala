b8479194af7b0ddda2efe25d695418bbe65c4463
koushikpuppala <koushikpuppala203@gmail.com>
2022-07-22 12:59:22 +0530

    Koushikpuppala

--- next.config.js
+++ next.config.js
@@ -3,20 +3,21 @@
 const withPWA = require('next-pwa')
 
 const nextConfig = withPWA({
-    reactStrictMode: true,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false
-            require('./scripts/sitemap-generator.js')
-        }
-        return config
-    },
-    pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development',
-    },
+	reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback.fs = false
+			require('./scripts/sitemap-generator.js')
+		}
+		return config
+	},
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === 'development',
+		importScripts: ['https://arc.io/arc-sw-core.js'],
+	},
 })
 
 module.exports = nextConfig
