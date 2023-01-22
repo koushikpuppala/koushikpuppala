40129e99f57192a7d7478bba5b74c1ea38af2e1b
Koushikpuppala <koushikpuppala203@gmail.com>
2023-01-22 11:57:33 +0530

    Koushikpuppala

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,15 +1,25 @@
+import { Header } from '@import/components'
 import type { NextPage } from 'next'
 
 const Resume: NextPage = () => {
 	return (
-		<iframe
-			style={{
-				border: 'none',
-				width: '100%',
-				height: '100%',
-				minHeight: '100vh',
-			}}
-			src='/Koushikpuppala Resume.pdf'></iframe>
+		<>
+			<Header title='Koushik Puppala | Resume' />
+			<h1
+				style={{
+					display: 'none',
+				}}>
+				Resume
+			</h1>
+			<iframe
+				style={{
+					border: 'none',
+					width: '100%',
+					height: '100%',
+					minHeight: '100vh',
+				}}
+				src='/Koushikpuppala Resume.pdf'></iframe>
+		</>
 	)
 }
 
--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -3,11 +3,12 @@ const fs = require('fs')
 const addPage = page => {
 	const path = page.replace('pages', '').replace('.tsx', '')
 	const route = path === '/index' ? '' : path
-	return `<url>
-    <loc>${`https://koushikpuppala.com/${route}`}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
+	return `
+	<url>
+		<loc>${`https://koushikpuppala.com${route}`}</loc>
+		<lastmod>${new Date().toISOString()}</lastmod>
+		<changefreq>monthly</changefreq>
+		<priority>1.0</priority>
 	</url>`
 }
 
@@ -17,7 +18,8 @@ const generateSitemap = async () => {
 	const pages = await globby(['pages/**/*.tsx', '!pages/_*.tsx', '!pages/api'])
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	${pages.map(page => addPage(page)).join('\n')}
+		${pages.map(page => addPage(page)).join('\n')}
+
 	</urlset>`
 	fs.writeFileSync('public/sitemap.xml', sitemap)
 }
