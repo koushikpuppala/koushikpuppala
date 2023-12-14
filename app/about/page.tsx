ffa7fab4e171ade9a14c4292152c255124ef6c9e
Koushikpuppala <koushikpuppala203@gmail.com>
2023-12-14 21:28:03 +0530

    Add ServiceCardComponent to AboutPage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-import { AboutComponent } from '@import/components'
+import { AboutComponent, ServiceCardComponent } from '@import/components'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -12,7 +12,17 @@ export const metadata: Metadata = {
 }
 
 const AboutPage = () => {
-	return <AboutComponent />
+	return (
+		<div className='h-full bg-primary/30'>
+			<div className='h-full w-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+				<div className='container mx-auto h-full items-center overflow-y-auto py-6 text-left md:py-12 lg:py-24'>
+					<AboutComponent />
+
+					<ServiceCardComponent />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AboutPage
