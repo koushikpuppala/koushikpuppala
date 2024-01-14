d89fefc42220de0523ddd5c5f77fa41dad734659
Koushikpuppala <koushikpuppala203@gmail.com>
2024-01-14 18:36:36 +0530

    chore: add pre-commit, commit-msg and update package.json
    Signed-off-by: Koushikpuppala <koushikpuppala203@gmail.com>

--- src/constant.ts
+++ src/constant.ts
@@ -9,7 +9,7 @@ import {
 	CollegeLogo,
 	TGHLogo,
 	SchoolLogo,
-} from '@import'
+} from '@import/images'
 import { FaLinkedinIn, FaGithub, FaDiscord, FaSkype } from 'react-icons/fa6'
 import { HiHome, HiUser, HiDocument, HiEnvelopeOpen, HiBriefcase, HiFolder } from 'react-icons/hi2'
 
