d3b08ef82d49423cd5d6ebd735cb918da32d18f6
Koushikpuppala <koushikpuppala203@gmail.com>
2024-05-16 16:30:04 +0530

    chore: update yarn dependencies to latest versions
    Signed-off-by: Koushikpuppala <koushikpuppala203@gmail.com>

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,6 +1,20 @@
 import { MetadataRoute } from 'next'
 import { globby } from 'globby'
 
+const readableDateTime = (date: string | number | Date) => {
+	return new Date(date).toLocaleString('en-US', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
+		weekday: 'long',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+		fractionalSecondDigits: 3,
+		timeZoneName: 'longGeneric',
+	})
+}
+
 const SitemapPage = async (): Promise<MetadataRoute.Sitemap> => {
 	const routes: MetadataRoute.Sitemap = []
 	const paths = (await globby(['app/**/page.tsx', '!app/api/**/*', '!app/**/[id]/*'])).map(path => {
@@ -8,36 +22,41 @@ const SitemapPage = async (): Promise<MetadataRoute.Sitemap> => {
 	})
 
 	paths.forEach(path => {
-		const priority = Number((1 - (path.split('/').length - 1) * 0.2).toFixed(1))
-		const changeFrequency =
-			priority === 1
-				? 'always'
-				: priority === 0.8
-					? 'hourly'
-					: priority === 0.6
-						? 'daily'
-						: priority === 0.4
-							? 'weekly'
-							: priority === 0.2
-								? 'monthly'
-								: priority === 0
-									? 'yearly'
-									: 'never'
+		const priority = 1 - (path.split('/').length - 1) * 0.2
+		const date = new Date()
 
-		const lastModified =
-			priority === 1
-				? new Date().toLocaleString()
-				: priority === 0.8
-					? new Date(new Date().setHours(new Date().getHours() - 1)).toLocaleString()
-					: priority === 0.6
-						? new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleString()
-						: priority === 0.4
-							? new Date(new Date().setDate(new Date().getDate() - 7)).toLocaleString()
-							: priority === 0.2
-								? new Date(new Date().setMonth(new Date().getMonth() - 1)).toLocaleString()
-								: priority === 0
-									? new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toLocaleString()
-									: undefined
+		let changeFrequency: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never'
+		let lastModified: string | undefined = undefined
+
+		switch (priority) {
+			case 1:
+				changeFrequency = 'always'
+				lastModified = readableDateTime(date)
+				break
+			case 0.8:
+				changeFrequency = 'hourly'
+				lastModified = readableDateTime(date.setHours(date.getHours() - 1))
+				break
+			case 0.6:
+				changeFrequency = 'daily'
+				lastModified = readableDateTime(date.setDate(date.getDate() - 1))
+				break
+			case 0.4:
+				changeFrequency = 'weekly'
+				lastModified = readableDateTime(date.setDate(date.getDate() - 7))
+				break
+			case 0.2:
+				changeFrequency = 'monthly'
+				lastModified = readableDateTime(date.setMonth(date.getMonth() - 1))
+				break
+			case 0:
+				changeFrequency = 'yearly'
+				lastModified = readableDateTime(date.setFullYear(date.getFullYear() - 1))
+				break
+			default:
+				changeFrequency = 'never'
+				break
+		}
 
 		routes.push({
 			url: `${process.env.NEXT_PUBLIC_VERCEL_URL}${path}`,
