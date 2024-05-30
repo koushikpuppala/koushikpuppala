6b89e1eeb8fe58ce577eae4ba3021c16ca046437
Koushikpuppala <koushikpuppala203@gmail.com>
2024-05-30 14:13:08 +0530

    chore: Update npm dependencies to latest versions
    Signed-off-by: Koushikpuppala <koushikpuppala203@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
+
+const addVariablesForColors = ({ addBase, theme }: any) => {
+	let allColors = flattenColorPalette(theme('colors'))
+	let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
+
+	addBase({ ':root': newVars })
+}
+
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
@@ -43,5 +54,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [addVariablesForColors],
 }
