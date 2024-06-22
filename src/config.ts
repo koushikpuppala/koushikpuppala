cdf05be30c4bc1a938b4cf28f582f2ab3bc50705
Koushikpuppala <koushikpuppala203@gmail.com>
2024-06-22 15:20:52 +0530

    chore: update config.ts to use secret environment variable for Google Analytics
    Signed-off-by: Koushikpuppala <koushikpuppala203@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -13,38 +13,8 @@ export const config = {
 	projectId: assertValue(process.env.NEXT_PUBLIC_SANITY_ID, 'Missing environment variable: NEXT_PUBLIC_SANITY_ID'),
 	sentryDsn: assertValue(process.env.NEXT_PUBLIC_SENTRY_DSN, 'Missing environment variable: NEXT_PUBLIC_SENTRY_DSN'),
 	deployUrl: assertValue(process.env.NEXT_PUBLIC_DEPLOY_URL, 'Missing environment variable: NEXT_PUBLIC_DEPLOY_URL'),
-	firebase: {
-		apiKey: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_API_KEY',
-		),
-		authDomain: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN',
-		),
-		databaseURL: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_DATABASE_URL',
-		),
-		projectId: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_PROJECT_ID',
-		),
-		storageBucket: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET',
-		),
-		messagingSenderId: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
-		),
-		appId: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_APP_ID',
-		),
-		measurementId: assertValue(
-			process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-			'Missing environment variable: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID',
-		),
-	},
+	googleAnalytics: assertValue(
+		process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+		'Missing environment variable: NEXT_PUBLIC_GOOGLE_ANALYTICS',
+	),
 }
